0774af791e7b1e189e9ac64b28b352aaa3bf5a65
swen2003 <78562196+swen2003@users.noreply.github.com>
2021-02-16 23:26:30 +0700

    fill bytes

--- test.sh
+++ test.sh
@@ -1,45 +1,89 @@
-printf "akan di mulai dalam 50 menit"
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "akan di dilanjutkan membuka file test1.sh dalam 50 menit"
 sleep 3000
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
-printf "tunggu 1 menit setiap 1 commit"
-sleep 60
-
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
-printf "tunggu 1 menit setiap 1 commit"
-sleep 60
-
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
-printf "tunggu 1 menit setiap 1 commit"
-sleep 60
-
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
-printf "tunggu 1 menit setiap 1 commit"
-sleep 60
-
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
 start test1.sh
